114eb4c2dab5e7c8589e1cf269862e69369757ba
ArtemyevaIA <nickart.artemyev@gmail.com>
2025-09-10 15:27:15 +0700

    betta_v1

--- zad.lua
+++ zad.lua
@@ -1,5 +1,5 @@
 script_name("zad")
-script_version("alpha_v1.0")
+script_version("betta_v1")
 
 local enable_autoupdate = true -- false to disable auto-update + disable sending initial telemetry (server, moonloader version, script version, samp nickname, virtual volume serial number)
 local autoupdate_loaded = false
@@ -49,7 +49,7 @@ function main()
     sampAddChatMessage('', -1)
     sampAddChatMessage('Скрипт работы с совместными заданиями {FFA500}успешно загружен', -1)
     sampAddChatMessage('Команда для открытия {FFA500}/zad', -1)
-    sampAddChatMessage('Разработал {FFA500}Ирин Крайн', -1)
+    --sampAddChatMessage('Разработал {FFA500}Ирин Крайн', -1)
     sampAddChatMessage('', -1)
 
     sampRegisterChatCommand('zad', zadmenu)
@@ -667,6 +667,7 @@ function main()
         end
         if check_update then
             sampProcessChatInput('/upd', -1)
+            wait(10000)
         end
     end
 end
