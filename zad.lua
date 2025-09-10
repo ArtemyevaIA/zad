7bfd6f6790bbe62e8f50a1fa9ef19d23766ef32e
ArtemyevaIA <nickart.artemyev@gmail.com>
2025-09-10 15:18:40 +0700

    3.1.1

--- zad.lua
+++ zad.lua
@@ -1,5 +1,5 @@
 script_name("zad")
-script_version("v3.1")
+script_version("v3.1.1")
 
 local enable_autoupdate = true -- false to disable auto-update + disable sending initial telemetry (server, moonloader version, script version, samp nickname, virtual volume serial number)
 local autoupdate_loaded = false
